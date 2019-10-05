ae5272ff6717f3a73338ca8360d47e5ba99bc9b5
bramdal <55013431+bramdal@users.noreply.github.com>
2019-10-06 00:04:25 +0530

    Add files via upload

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+
+    AudioSource[] audioSources;
+    AudioSource masterBeat;
+    AudioSource commandMutedBeat;
+    AudioSource beatMissSigh;
+    AudioSource beatSkipSigh;
+
+    AudioSource drumTop;
+    AudioSource drumRight;
+    AudioSource drumBottom;
+    AudioSource drumLeft;
+
+    [Header("Public references")]
+    public TeamController teamController;
+    bool allowedToBeat;
+    //beat track variables
+    [Header("Beat timing variables")]
+    [Range(0, 120)]
+    public float beatsPerMinute = 80;
+    [Range(0, 1)]
+    public float errorMarginTime = .3f;
+
+    
+    int[] commandType;
+    int commandCount = 0;
+    int inactiveBeatCount = 0;  //how many beats after command are inactive
+
+    //measure how long an active beat time has no input
+    float beatFallTime;
+    
+    //count how long beat is active without an input
+    private float beatActiveTime = 0f; 
+    new private bool enabled;
+    public bool hasBeatInput{
+        get{
+            return enabled;
+        }
+        set{
+            enabled = value;
+            if(!enabled)
+                beatActiveTime = Time.time;
+        }
+    }
+    
+    bool lastBeatHasInput = true;   //true means no, false means yes, used with offset along with hasBeatInput
+
+    //ui flash colour variables
+    Color flashColor = new Color(255f, 255f, 255f, 1);
+    [Header("Sprite Display Variables")]
+    public Image drumTopSprite;
+    public Image drumRightSprite;
+    public Image drumBottomSprite;
+    public Image drumLeftSprite;
+    Image currentDrumSprite = null;
+    [Range(0.25f , 0.5f)]
+    public float spriteFlashTime = 0.5f;
+
+    //fever variables
+    bool fever;
+    float feverTimeHold;
+    public Image feverSprite;
+    
+
+
+    void Start()
+    {
+        allowedToBeat = true;
+        hasBeatInput = false;
+
+        inactiveBeatCount = 0;
+
+        float invokeTime  = 60f / beatsPerMinute;
+        teamController.secondsToBeats = (int)(invokeTime * 4);
+
+        commandType = new int[4]{0, 0, 0, 0};
+
+        audioSources = GetComponents<AudioSource>();
+        masterBeat = audioSources[0];
+        commandMutedBeat = audioSources[1];
+        beatMissSigh = audioSources[2];
+        beatSkipSigh = audioSources[3];
+        
+        drumTop = audioSources[4];
+        drumRight = audioSources[5];
+        drumBottom = audioSources[6];
+        drumLeft = audioSources[7];
+        
+        beatFallTime = errorMarginTime;
+
+        InvokeRepeating("PlayMasterBeat", errorMarginTime/2f, invokeTime);
+        InvokeRepeating("AllowBeat", 0f, invokeTime);
+    }
+
+   
+    void Update()
+    {
+        beatFallTime -= Time.deltaTime;
+            if(beatFallTime <0f){
+                allowedToBeat = false;
+                
+            if(commandType[3] != 0){
+                bool commandMatched = SetInput(commandType);
+                if(commandMatched){
+                    commandCount++;
+                    inactiveBeatCount = 4;      //4 beats after input are inactive
+                }       
+                else{
+                    inactiveBeatCount = 0; 
+                    commandCount = 0;
+                }       
+                Array.Clear(commandType, 0, commandType.Length);
+            }    
+        }    
+
+        if(allowedToBeat && hasBeatInput && Input.anyKeyDown){      //double beat per master beat
+                print("double beat not allowed");
+                hasBeatInput = false;
+                lastBeatHasInput = true;
+                Array.Clear(commandType, 0, commandType.Length);
+        }
+
+        GetDrumInputs();
+           
+        if(!allowedToBeat && Input.anyKeyDown){                     //mistiming beat with master beat
+            beatMissSigh.Play();
+            Array.Clear(commandType, 0, commandType.Length);
+            commandCount = 0;
+        }  
+        
+        if(inactiveBeatCount >0 && Input.anyKeyDown){               //interrupting command
+            Array.Clear(commandType, 0, commandType.Length);
+            commandCount = 0;
+            //do physical motion stop here
+        }
+    
+        
+        if(Time.time - beatActiveTime >= errorMarginTime && lastBeatHasInput && allowedToBeat){      //skipping a master beat
+            lastBeatHasInput = true;
+            Array.Clear(commandType, 0, commandType.Length);
+            
+        }
+
+       if(currentDrumSprite != null){
+           Image temporaryReference = currentDrumSprite;
+           temporaryReference.color = Color.Lerp(currentDrumSprite.color, Color.clear, spriteFlashTime);
+       }
+
+
+        //continuos beats required to maintain fever
+        if(commandCount >=4 ){
+            fever = true;
+            feverSprite.gameObject.SetActive(true);
+        }    
+
+        if(inactiveBeatCount>=0){
+            feverTimeHold = Time.time;
+        }
+        if(Time.time - feverTimeHold >= ((errorMarginTime)*2) + 1f && fever ){    
+            commandCount = 0;
+            fever = false;
+            feverSprite.gameObject.SetActive(false);
+        }    
+    }
+    
+
+    void AllowBeat(){
+        beatFallTime = errorMarginTime;
+
+        if(inactiveBeatCount == 0)
+            teamController.resetSpritesToIdle();
+            
+        allowedToBeat = true; 
+        
+        if(hasBeatInput){
+            hasBeatInput = false;
+        }
+    }
+    void PlayMasterBeat(){
+        if((inactiveBeatCount--)>0){
+            commandMutedBeat.Play();
+        }      
+        else
+            masterBeat.Play();    
+    }
+
+    bool SetInput(int[] commandType){
+        bool commandMatched = teamController.GetInput(commandType);
+        return commandMatched;
+    }
+
+    void GetDrumInputs(){
+        if(allowedToBeat){
+            if(commandType[0] == 0){
+                if(Input.GetButtonDown("Left")){
+                    commandType[0] = 1;
+                    hasBeatInput = true;
+                    drumLeft.Play();
+                    currentDrumSprite = drumLeftSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Right")){ 
+                    commandType[0] = 2;
+                    hasBeatInput = true;
+                    drumRight.Play();
+                    currentDrumSprite = drumRightSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Top")){ 
+                    commandType[0] = 3; 
+                    hasBeatInput = true;
+                    drumTop.Play();
+                    currentDrumSprite = drumTopSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Down")){ 
+                    commandType[0] = 4;            
+                    hasBeatInput = true;
+                    drumBottom.Play();
+                    currentDrumSprite = drumBottomSprite;
+                    currentDrumSprite.color = flashColor;
+                    }
+            }
+            else if(commandType[1] == 0){
+                if(Input.GetButtonDown("Left")){
+                    commandType[1] = 1;
+                    hasBeatInput = true;
+                    drumLeft.Play();
+                    currentDrumSprite = drumLeftSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Right")){ 
+                    commandType[1] = 2;
+                    hasBeatInput = true;
+                    drumRight.Play();
+                    currentDrumSprite = drumRightSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Top")){ 
+                    commandType[1] = 3; 
+                    hasBeatInput = true;
+                    drumTop.Play();
+                    currentDrumSprite = drumTopSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Down")){ 
+                    commandType[1] = 4;  
+                    hasBeatInput = true;
+                    drumBottom.Play();
+                    currentDrumSprite = drumBottomSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+            }
+            else if(commandType[2] == 0){
+                if(Input.GetButtonDown("Left")){
+                    commandType[2] = 1;
+                    hasBeatInput = true;
+                    drumLeft.Play();
+                    currentDrumSprite = drumLeftSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Right")){ 
+                    commandType[2] = 2;
+                    hasBeatInput = true;
+                    drumRight.Play();
+                    currentDrumSprite = drumRightSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Top")){ 
+                    commandType[2] = 3; 
+                    hasBeatInput = true;
+                    drumTop.Play();
+                    currentDrumSprite = drumTopSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Down")){ 
+                    commandType[2] = 4;  
+                    hasBeatInput = true;
+                    drumBottom.Play();
+                    currentDrumSprite = drumBottomSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+            }
+            else if(commandType[3] == 0){
+                if(Input.GetButtonDown("Left")){
+                    commandType[3] = 1;
+                    hasBeatInput = true;
+                    drumLeft.Play();
+                    currentDrumSprite = drumLeftSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Right")){ 
+                    commandType[3] = 2;
+                    hasBeatInput = true;
+                    drumRight.Play();
+                    currentDrumSprite = drumRightSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Top")){ 
+                    commandType[3] = 3; 
+                    hasBeatInput = true;
+                    drumTop.Play();
+                    currentDrumSprite = drumTopSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+                else if(Input.GetButtonDown("Down")){ 
+                    commandType[3] = 4;
+                    hasBeatInput = true;
+                    drumBottom.Play();
+                    currentDrumSprite = drumBottomSprite;
+                    currentDrumSprite.color = flashColor;
+                }
+            }
+        }
+        lastBeatHasInput = !hasBeatInput;
+    }    
+}
---
+++ Assets/TeamController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TeamController : MonoBehaviour
+{
+
+    AudioSource unmatchedCommand;
+    Animator sprite1Animator;
+    Animator sprite2Animator;
+    Animator[] spriteAnimators;
+    Rigidbody2D thisRigidbody;
+
+    [HideInInspector]public int secondsToBeats;
+
+    //movement variables
+    [Header("Movement Variables")]
+    public float jumpHeight = 2f;
+    public float moveDistance = 3f;
+
+    [Header("Image references")]
+    public Image unmatchedCommandSprite;
+    float spriteFlashSpeed = 0.5f;
+    Color flashColor = new Color(255f, 255f, 255f, 1);
+
+    void Start()
+    {
+        thisRigidbody = GetComponent<Rigidbody2D>();
+
+        unmatchedCommand = GetComponent<AudioSource>();
+
+        spriteAnimators = GetComponentsInChildren<Animator>();
+        sprite1Animator = spriteAnimators[0];
+        sprite2Animator = spriteAnimators[1];
+    }
+
+    private void Update() {
+        if(unmatchedCommandSprite.color != Color.clear)
+            unmatchedCommandSprite.color = Color.Lerp(unmatchedCommandSprite.color, Color.clear, spriteFlashSpeed);
+    }
+    public bool GetInput(int[] commandType){
+       
+        //walk
+        if(ArrayCompare(commandType, new int[]{1, 1, 1, 2})){
+            Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y); 
+            thisRigidbody.DOMove(currentPosition + (Vector2.left * moveDistance) , secondsToBeats, false );
+
+            sprite1Animator.SetBool("Walking", true);
+            sprite2Animator.SetBool("Walking", true);
+
+            return true;
+        }
+        //jump
+        else if(ArrayCompare(commandType, new int[]{3, 3, 1, 2})){
+            Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+            thisRigidbody.DOJump(currentPosition + (Vector2.up * jumpHeight), 0.5f, 0, secondsToBeats, false );
+
+            sprite1Animator.SetBool("Jumping", true);
+            sprite2Animator.SetBool("Jumping", true);
+            
+            return true;
+        }
+        else{
+            unmatchedCommand.Play();
+            unmatchedCommandSprite.color = flashColor;
+            return false;
+        }
+    }
+
+    bool ArrayCompare(int[] array1, int[] array2){
+        if(array1.Length != array2.Length)
+            return false;
+        for (var i = 0; i < array1.Length; i++){
+            if(array1[i] != array2[i])
+                return false;
+        }    
+        return true;
+    }
+
+    public void resetSpritesToIdle(){
+        sprite1Animator.SetBool("Walking", false);
+        sprite2Animator.SetBool("Walking", false);
+
+        sprite1Animator.SetBool("Jumping", false);
+        sprite2Animator.SetBool("Jumping", false);
+    }
+
+    
+}
+
